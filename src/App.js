d92e706b9c562b72445055b93b40193b07f8d3a3
ronit-panchal <ronitpanchal854@gmail.com>
2025-03-12 20:28:01 +0530

    new changes

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <h1>Ronit Panchal</h1>
+        <h2>React Developer</h2>
+        <h3>Software Developer</h3>        
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
